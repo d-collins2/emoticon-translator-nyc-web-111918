79987ea7258eab38f48195d84de24a3df2145419
Damon Collins <damon.c.p.collins@gmail.com>
2018-11-09 09:36:28 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,10 +17,10 @@ def load_library(file_path)
   emoticon_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(path, emoticon)
   # code goes here
 end
